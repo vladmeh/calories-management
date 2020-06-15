703488d071d5b6fdff8cd565d4dab27cf2e44429
vladmeh <vladmeh@gmail.com>
2020-06-16 00:26:40 +0300

    HW02_4_layers_app

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,39 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
+
+@Service
 public class MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
 
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    public Meal create(int userId, Meal meal) {
+        return repository.save(userId, meal);
+    }
+
+    public void update(int userId, Meal meal) {
+        checkNotFoundWithId(repository.save(userId, meal), meal.getId());
+    }
+
+    public void delete(int userId, int mealId) {
+        checkNotFoundWithId(repository.delete(userId, mealId), mealId);
+    }
+
+    public Meal get(int userId, int mealId) {
+        return checkNotFoundWithId(repository.get(userId, mealId), mealId);
+    }
+
+    public List<Meal> getAll(int userId) {
+        return repository.getAll(userId);
+    }
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,55 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+
+@Controller
 public class MealRestController {
-    private MealService service;
+    private static final Logger log = LoggerFactory.getLogger(MealRestController.class);
+
+    private final MealService service;
+
+    @Autowired
+    public MealRestController(MealService service) {
+        this.service = service;
+    }
+
+    private static int getUserId() {
+        return SecurityUtil.authUserId();
+    }
+
+    public List<MealTo> getAll() {
+        log.info("getAll for user {}", getUserId());
+        return MealsUtil.getTos(service.getAll(getUserId()), MealsUtil.DEFAULT_CALORIES_PER_DAY);
+    }
+
+    public Meal get(int id) {
+        log.info("get {} for user {}", id, getUserId());
+        return service.get(getUserId(), id);
+    }
+
+    public Meal create(Meal meal) {
+        log.info("create {} for user {}", meal, getUserId());
+        checkNew(meal);
+        return service.create(getUserId(), meal);
+    }
+
+    public void update(Meal meal, int id) {
+        log.info("update {} with id={} for user {}", meal, id, getUserId());
+        assureIdConsistent(meal, id);
+        service.update(getUserId(), meal);
+    }
 
 }
