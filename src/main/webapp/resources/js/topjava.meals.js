16d12f85a0320ae9389c17d62a69483b010e9e95
vladmeh <vladmeh@gmail.com>
2020-08-19 00:10:18 +0300

    10_04_opt json view

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -17,13 +17,7 @@ $(function () {
     makeEditable(mealAjaxUrl, {
         "columns": [
             {
-                "data": "dateTime",
-                "render": function (date, type, row) {
-                    if (type === 'display') {
-                        return formatDate(date);
-                    }
-                    return date;
-                }
+                "data": "dateTimeUI",
             },
             {
                 "data": "description"
